947cbc4c18951265408d64712eaf763ccc2fa7ec
Pedro G. Dias <pedro@digitaldias.com>
2020-02-03 12:07:44 +0100

    Create a skeleton PottyBreakRepository

---
+++ webapi/PuppyApi/Data/PottyBreakRepository.cs
@@ -0,0 +1,36 @@
+﻿using PuppyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PuppyApi.Data
+{
+    public class PottyBreakRepository : IPottyBreakRepository
+    {
+        public PottyBreakRepository()
+        {
+
+        }
+
+        public Task DeleteAsync(PottyBreak pottyBreak)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PottyBreak>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PottyBreak> GetById(Guid verifiedGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(PottyBreak pottyBreak)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
