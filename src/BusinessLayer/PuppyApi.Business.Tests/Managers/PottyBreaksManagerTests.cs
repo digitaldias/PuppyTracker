cc11985971e1ae9a89b733d65217db284245b69d
Pedro G. Dias <pedro@digitaldias.com>
2020-02-15 10:40:06 +0100

    Start working on Unit-Tests for the PottyBreaksManager

---
+++ src/BusinessLayer/PuppyApi.Business.Tests/Managers/PottyBreaksManagerTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using PuppyApi.Business.Managers;
+using PuppyApi.CrossDomain.TestHelpers;
+using PuppyApi.Domain.Contracts.Handlers;
+using PuppyApi.Domain.Contracts.Repositories;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace PuppyApi.Business.Tests.Managers
+{
+    public class PottyBreaksManagerTests : TestsFor<PottyBreaksManager>
+    {
+        [Fact]
+        public void Constructor_ExceptionHandlerIsNull_ThrowsExeption()
+        {
+            // Arrange
+            IExceptionHandler nullHandler = null;
+            var repoMock = new Mock<IPottyBreakRepository>();
+            
+            // Act
+            Assert.Throws<ArgumentNullException>(() => Instance = new PottyBreaksManager(nullHandler, repoMock.Object));
+        }
+
+        [Fact]
+        public void Constructor_RepositoryIsNull_ThrowsExeption()
+        {
+            // Arrange
+            var exeHandlerMock = new Mock<IExceptionHandler>();
+            IPottyBreakRepository nullRepo = null;
+
+            // Act
+            Assert.Throws<ArgumentNullException>(() => Instance = new PottyBreaksManager(exeHandlerMock.Object, nullRepo));
+        }
+
+        [Fact]
+        public void Constructor_ParametersAreValid_DoesNotThrow()
+        {
+            // ASsert            
+            Instance.Should().NotBeNull();
+        }
+    }
+}
