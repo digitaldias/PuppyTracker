97a5f235cd37242b39225c3afcb2b612a7557fe4
Pedro G. Dias <pedro@digitaldias.com>
2020-02-15 10:39:29 +0100

    Replace MoqAutomocker Nuget with my own implementation

---
+++ src/CrossDomainLayer/PuppyApi.CrossDomain.TestHelpers/AutoMocking/MoqAutoMocker.cs
@@ -0,0 +1,12 @@
+﻿namespace StructureMap.AutoMocking.Moq
+{
+    using AutoMocking;
+
+    public class MoqAutoMocker<T> : AutoMocker<T> where T : class
+    {
+        public MoqAutoMocker() : base(new MoqServiceLocator())
+        {
+            ServiceLocator = new MoqServiceLocator();
+        }
+    }
+}
