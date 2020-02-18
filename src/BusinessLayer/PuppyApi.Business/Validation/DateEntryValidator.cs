baada376af65a309884aa05d9daa607bdd0a9f39
Pedro G. Dias <pedro@digitaldias.com>
2020-02-18 06:59:34 +0100

    Add comment to DateEntryValidator

--- src/BusinessLayer/PuppyApi.Business/Validation/DateEntryValidator.cs
+++ src/BusinessLayer/PuppyApi.Business/Validation/DateEntryValidator.cs
@@ -5,6 +5,11 @@ namespace PuppyApi.Business.Validation
 {
     public class DateEntryValidator : IValidator<DateTime>
     {
+        /// <summary>
+        /// Current rules: No future dates, and a maximum of 1 day old entries
+        /// </summary>
+        /// <param name="dateTime">The DateTime to inspect</param>
+        /// <returns>true if validation rules pass</returns>
         public bool IsValid(DateTime dateTime)
         {
             var difference = dateTime - DateTime.Now;
